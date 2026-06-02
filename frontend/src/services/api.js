152bd3f5fe1a1341100f00bfb2cb4d70436b456c
Asma Shahzadi <asmashahzadi764@gmail.com>
2026-06-02 23:20:22 +0500

    Update API base URL to production environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://project-management-tool-38dj.onrender.com/api",
 });
 
 // Request interceptor to add token
